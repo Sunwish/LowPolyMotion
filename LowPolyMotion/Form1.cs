544fcbccc146054a43f373c73f6ed154d8df2987
Sunwish <isunwish@qq.com>
2019-09-07 12:24:47 +0800

    1. Add Helper.cs, define and implement some necessary structs and classes. 2. Implement grid auto-fit, resize the window to preview.

--- LowPolyMotion/Form1.cs
+++ LowPolyMotion/Form1.cs
@@ -8,39 +8,93 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
         private System.Timers.Timer timer = new System.Timers.Timer();
+        private LowPolyMotion.GridConfig grid = new LowPolyMotion.GridConfig();
+        private LowPolyMotion.Rect[,] rects;
         private int i = 10;
+        private int c_x, c_y, c_w, c_h; // x方向网格数, y方向网格数, x方向网格宽度, y方向网格高度
+
         public Form1()
         {
             InitializeComponent();
             SetStyle(ControlStyles.UserPaint, true);
             SetStyle(ControlStyles.AllPaintingInWmPaint, true); // 禁止擦除背景
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true); // 开启双缓冲
+            InitializeLowPolyConfig();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             timer.Elapsed += timer_event;
+            this.SizeChanged += Form1_SizeChanged;
+        }
+
+        private void InitializeLowPolyConfig()
+        {
+            // Initialize Grid
+            grid.initGridConfig(this.Size.Width - 16, this.Size.Height - 46, 10);
+
+            // Initialize Rects
+            c_x = grid.getCrossCellCount();
+            c_y = grid.getDirectionCellCount();
+            c_w = grid.getCrossCellWidth();
+            c_h = grid.getDirectionCellHeight();
+            rects = new LowPolyMotion.Rect[c_x, c_y];
+            for (int i = 0; i < c_x; i++)
+            {
+                for (int j = 0; j < c_y; j++)
+                {
+                    rects[i, j] = new LowPolyMotion.Rect();
+                    rects[i, j].xyz = new LowPolyMotion.xyz(c_w * i, c_h * j, 0);
+                    rects[i, j].w = c_w;
+                    rects[i, j].h = c_h;
+                    rects[i, j].T_Diagonal = new Random().Next(0, 2); // [0,2)
+                }
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            InitializeLowPolyConfig();
+            Draw();
         }
 
         private void timer_event(object sender, System.Timers.ElapsedEventArgs e)
         {
+            Draw();
+        }
+
+        private void Draw()
+        {
             // 在内存中建立虚拟画布
-            Bitmap bitmap = new Bitmap(this.Width, this.Height);
+            Bitmap bitmap = new Bitmap(this.Size.Width, this.Size.Height);
+
             // 获取内存画布的 Graphics 引用
             Graphics g = Graphics.FromImage(bitmap);
+            g.Clear(Color.White);
+
             // 创建画笔
             Pen myPen = new Pen(Color.Black, 2);
+
             // 绘图
-            g.Clear(Color.White);
-            g.DrawLine(myPen, 100, 200 - 50 * (float)Math.Sin(Math.PI * i / 180), 300, 200 + 50 * (float)Math.Sin(Math.PI * i / 180));
+            for (int i = 0; i < c_x + 1; i++)
+            {
+                g.DrawLine(myPen, c_w * i, 0, c_w * i, c_h * c_y);
+            }
+            for (int j = 0; j < c_y + 1; j++)
+            {
+                g.DrawLine(myPen, 0, c_h * j, c_w * c_x, c_h * j);
+            }
+
             // 将内存画布绘制到屏幕上
             Graphics myGraphics = this.CreateGraphics();
             myGraphics.DrawImage(bitmap, 0, 0);
+
             /*
             Brush myBrush = new SolidBrush(Color.BlueViolet);
             Point[] myPoints = new Point[3];
@@ -49,8 +103,10 @@ namespace WindowsFormsApp1
             myPoints[2].X = 100 + moveLength; myPoints[2].Y = 100 + moveLength;
             g.FillPolygon(myBrush, myPoints);
             */
-            i += 10;
+            // i += 10;
 
+
+            // 释放资源
             bitmap.Dispose();
             g.Dispose();
             myPen.Dispose();
@@ -59,9 +115,13 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            /*
             timer.Interval = 5;
             timer.Start();
+            */
+            InitializeLowPolyConfig();
+            Draw();
+
         }
     }
 }
---
+++ LowPolyMotion/Helper.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LowPolyMotion
+{
+    class xyz
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public xyz(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public xyz(xyz xyz)
+        {
+            this.x = xyz.x;
+            this.y = xyz.y;
+            this.z = xyz.z;
+        }
+
+        // 运算符重载
+        public static xyz operator +(xyz xyz1, xyz xyz2)
+        {
+            xyz result = new xyz(xyz1.x + xyz2.x, xyz1.y + xyz2.y, xyz1.z + xyz2.z);
+            return result;
+        }
+
+        public static xyz operator -(xyz xyz1, xyz xyz2)
+        {
+            xyz result = new xyz(xyz1.x - xyz2.x, xyz1.y - xyz2.y, xyz1.z - xyz2.z);
+            return result;
+        }
+
+        public static xyz operator *(xyz xyz, float param)
+        {
+            xyz result = new xyz(xyz.x * param, xyz.y * param, xyz.z * param);
+            return result;
+        }
+    }
+
+    class Vector
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector() { }
+        public Vector(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector(Vector vector)
+        {
+            this.x = vector.x;
+            this.y = vector.y;
+            this.z = vector.z;
+        }
+
+        // 运算符重载
+        public static Vector operator +(Vector vector1, xyz vector2)
+        {
+            Vector result = new Vector(vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z);
+            return result;
+        }
+
+        public static Vector operator -(Vector vector1, xyz vector2)
+        {
+            Vector result = new Vector(vector1.x - vector2.x, vector1.y - vector2.y, vector1.z - vector2.z);
+            return result;
+        }
+
+        public static Vector operator *(Vector vector, float param)
+        {
+            Vector result = new Vector(vector.x * param, vector.y * param, vector.z * param);
+            return result;
+        }
+    }
+
+    class PointF
+    {
+        public int x;
+        public int y;
+
+        public PointF(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // 运算符重载
+        public static PointF operator +(PointF point1, PointF point2)
+        {
+            PointF result = new PointF(point1.x + point2.x, point1.y + point2.y);
+            return result;
+        }
+
+        public static PointF operator -(PointF point1, PointF point2)
+        {
+            PointF result = new PointF(point1.x - point2.x, point1.y - point2.y);
+            return result;
+        }
+    }
+
+    class RGB
+    {
+        public int R, G, B;
+    }
+
+    class Rect
+    {
+        public xyz xyz = new xyz(0, 0, 0);
+        public float w;
+        public float h;
+        public int T_Diagonal; // 对角线类型 (0.\ 1./)
+        public Vector[] vector = new Vector[2]; // [0].左边面, [1].右边面
+        public xyz[] outcentreXYZ = new xyz[2]; // 三角面外心坐标, 规则同 vector
+        public RGB[] color = new RGB[2]; // 规则同 vector
+    }
+
+    class MotionData
+    {
+        public int n;
+        public int i;
+        public xyz begin_xyz;
+        public xyz target_xyz;
+        private xyz current_xyz;
+
+        public MotionData(int n, int i, xyz begin_xyz, xyz target_xyz)
+        {
+            this.n = n;
+            this.i = i;
+            this.begin_xyz = begin_xyz;
+            this.target_xyz = target_xyz;
+            this.current_xyz = begin_xyz;
+        }
+
+        public Boolean Next()
+        {
+            this.i++;
+
+            xyz deltaXYZ = new xyz(target_xyz - begin_xyz);
+            float process = (float)i / n;
+
+            this.current_xyz = this.begin_xyz + deltaXYZ * process;
+            return this.i == this.n;
+        }
+    }
+
+    class GridConfig
+    {
+        private int crossCellWidth;
+        private int directionCellHeight;
+        private int crossCellCount;
+        private int directionCellCount;
+
+        public GridConfig() { }
+        public GridConfig(int crossCellWidth, int directionCellHeight, int crossCellCount, int directionCellCount)
+        {
+            this.crossCellWidth = crossCellWidth;
+            this.directionCellHeight = directionCellHeight;
+            this.crossCellCount = crossCellCount;
+            this.directionCellCount = directionCellCount;
+        }
+
+        public void initGridConfig(int boardWidth, int boardHeight, int pointCount)
+        {
+            // 粗算均分数
+            float _crossCellCount = (float)Math.Sqrt((float)boardWidth / pointCount);
+            float _directionCellCount = (float)Math.Sqrt((float)boardHeight / pointCount);
+            if(_crossCellCount * _directionCellCount > pointCount)
+            {
+                do
+                {
+                    _crossCellCount--;
+                    _directionCellCount--;
+                } while (_crossCellCount * _directionCellCount > pointCount);
+                _crossCellCount++;
+                _directionCellCount++;
+            }
+            else if(_crossCellCount * _directionCellCount < pointCount)
+            {
+                do
+                {
+                    _crossCellCount++;
+                    _directionCellCount++;
+                } while (_crossCellCount * _directionCellCount < pointCount);
+            }
+
+            // 求最小误差对应的均分数
+            int[] r_p = new int[4];
+            int[] temp = new int[5];
+            r_p[0] = (int)_crossCellCount * (int)_directionCellCount;
+            r_p[1] = (int)Math.Round(_crossCellCount) * (int)_directionCellCount;
+            r_p[2] = (int)_crossCellCount * (int)Math.Round(_directionCellCount);
+            r_p[3] = (int)Math.Round(_crossCellCount) * (int)Math.Round(_directionCellCount);
+            temp[4] = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                temp[i] = Math.Abs(r_p[i] - pointCount);
+                if (temp[i] <= temp[temp[4]]) temp[4] = i;
+            }
+            switch(temp[4])
+            {
+                case 0:
+                    this.crossCellCount = (int)_crossCellCount;
+                    this.directionCellCount = (int)_directionCellCount;
+                    break;
+                case 1:
+                    this.crossCellCount = (int)Math.Round(_crossCellCount);
+                    this.directionCellCount = (int)_directionCellCount;
+                    break;
+                case 2:
+                    this.crossCellCount = (int)_crossCellCount;
+                    this.directionCellCount = (int)Math.Round(_directionCellCount);
+                    break;
+                default:
+                    this.crossCellCount = (int)Math.Round(_crossCellCount);
+                    this.directionCellCount = (int)Math.Round(_directionCellCount);
+                    break;
+            }
+
+            System.Diagnostics.Debug.WriteLine("点数变更:");
+            System.Diagnostics.Debug.WriteLine(this.crossCellCount * this.directionCellCount - pointCount);
+
+            System.Diagnostics.Debug.WriteLine("网格划分:");
+            System.Diagnostics.Debug.WriteLine("x: " + this.crossCellCount.ToString() + ", y: " + this.directionCellCount.ToString());
+
+            // 网格宽高
+            this.crossCellWidth = boardWidth / this.crossCellCount;
+            this.directionCellHeight = boardHeight / this.directionCellCount;
+        }
+
+
+        public int getCrossCellWidth()
+        {
+            return this.crossCellWidth;
+        }
+
+        public int getDirectionCellHeight()
+        {
+            return this.directionCellHeight;
+        }
+
+        public int getCrossCellCount()
+        {
+            return this.crossCellCount;
+        }
+
+        public int getDirectionCellCount()
+        {
+            return this.directionCellCount;
+        }
+    }
+}
